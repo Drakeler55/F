066d77e46684fea1c0b86737812a66f34cdf541b
Drakeler <50342325+Drakeler55@users.noreply.github.com>
2020-01-14 21:16:01 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   getThemes () {
     return {
-      'plugin-light-mode': {
+      'plugin-Galaxy-mode': {
         cssPath: 'styles/light.css',
         darkMode: false
       }
